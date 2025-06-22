eff4105a8372ea2f11d3585af87302f6373eef0f
이범호 <96612703+programbaam@users.noreply.github.com>
2025-06-22 14:53:19 +0900

    Feat: 백준 2557

---
+++ boj2557/boj2557.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace boj2557
+{
+    class boj2557
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
